1255abdc9fa470ae0fd024b88054409c2eba9171
netstalk3r <netstalk3r@gmail.com>
2014-07-15 10:08:23 +0300

    added trim to parsing dates

--- verifier/src/main/java/com/akvelon/ets/verifier/util/Holidays.java
+++ verifier/src/main/java/com/akvelon/ets/verifier/util/Holidays.java
@@ -37,13 +37,13 @@ public class Holidays {
 		String[] dates = out.toString().split(DELIMETER_FOR_DATES);
 		for (String date : dates) {
 			String[] splittedDate = date.split(DELIMETER_FOR_DATE);
-			int month = Integer.parseInt(splittedDate[1]) - 1;
+			int month = Integer.parseInt(splittedDate[1].trim()) - 1;
 			if (holidays.get(month) == null) {
 				List<Integer> days = new ArrayList<Integer>(3);
-				days.add(Integer.parseInt(splittedDate[0]));
+				days.add(Integer.parseInt(splittedDate[0].trim()));
 				holidays.put(month, days);
 			} else {
-				holidays.get(month).add(Integer.parseInt(splittedDate[0]));
+				holidays.get(month).add(Integer.parseInt(splittedDate[0].trim()));
 			}
 		}
 	}
