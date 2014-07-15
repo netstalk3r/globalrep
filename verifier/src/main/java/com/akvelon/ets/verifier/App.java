02970e917a8030f3e75f58753244506a15344d80
netstalk3r <netstalk3r@gmail.com>
2014-07-15 12:37:42 +0300

    add version and build number in file property in order to see them during program  executing

--- verifier/src/main/java/com/akvelon/ets/verifier/App.java
+++ verifier/src/main/java/com/akvelon/ets/verifier/App.java
@@ -1,5 +1,6 @@
 package com.akvelon.ets.verifier;
 
+import java.io.BufferedInputStream;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
@@ -12,8 +13,21 @@ public class App {
 	private static final Logger log = Logger.getLogger(App.class);
 
 	private static final String CREDENTIAL_FILE = "credentials.properties";
+	private static final String VERSION_FILE = "version.properties";
 
 	public static void main(String[] args) throws Exception {
+
+		Properties versionProps = new Properties();
+		BufferedInputStream is = null;
+		try {
+			is = new BufferedInputStream(App.class.getClassLoader().getResourceAsStream(VERSION_FILE));
+			versionProps.load(is);
+		} finally {
+			is.close();
+		}
+
+		log.info("ETS verifier v" + versionProps.getProperty("project.version"));
+		log.info("ETS verifier build number " + versionProps.getProperty("build.number"));
 
 		log.info("Load credentials...");
 		Properties credentialProperties = Util.loadProperties(CREDENTIAL_FILE);
