7803b458f66fc2e84dd7bfd605f663787a82b19c
netstalk3r <netstalk3r@gmail.com>
2019-01-05 22:10:07 +0200

    add scheduler service

---
+++ oauth2-password-behind-zuul/scheduler-service/src/main/java/com/paem/Scheduler.java
@@ -0,0 +1,21 @@
+package com.paem;
+
+import com.cmlatitude.annotation.EnableOAuth2SystemClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableOAuth2SystemClient
+@EnableResourceServer
+@EnableScheduling
+@EnableFeignClients
+public class Scheduler {
+    public static void main(String[] args) {
+        SpringApplication.run(Scheduler.class, args);
+    }
+}
---
+++ oauth2-password-behind-zuul/scheduler-service/src/main/java/com/paem/SchedulerConfiguration.java
@@ -0,0 +1,31 @@
+package com.paem;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Configuration
+public class SchedulerConfiguration {
+
+    public class SchedulingWork {
+
+        @Autowired
+        private RestService1Proxy restService1Proxy;
+
+        @Scheduled(fixedDelay = 30000)
+        public void doWork() {
+            try {
+                System.out.println(restService1Proxy.secure());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Bean
+    public SchedulingWork schedulingWork() {
+        return new SchedulingWork();
+    }
+
+}
